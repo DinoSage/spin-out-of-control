023d786634519d997bc8750f45a6d452f262d7a5
DinoSage <dinosage5632@gmail.com>
2022-04-21 15:11:43 -0700

    Minor Code Fix

--- SpinOutOfControl/Assets/Scripts/Rotate.cs
+++ SpinOutOfControl/Assets/Scripts/Rotate.cs
@@ -47,7 +47,6 @@ public class Rotate : MonoBehaviour
 
             timeElapsed = 0f;
             rotating = true;
-            Debug.Log(transform.rotation.eulerAngles.z);
             prerotationZ = transform.eulerAngles.z;
         }
 
