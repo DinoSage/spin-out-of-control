d4ce0e30244676c043176fe3d66f8e6c7604ac98
DinoSage <dinosage5632@gmail.com>
2022-05-19 22:33:02 -0700

    Test Level + Rotation Fix
    Rotation Fixed, Created Test Level w/ Polygon Collider 2D

--- SpinOutOfControl/Assets/Scripts/Rotate.cs
+++ SpinOutOfControl/Assets/Scripts/Rotate.cs
@@ -4,81 +4,33 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public float lerpDuration = 1;
-    public const float gravity = -9.81f;
-    public float endValueMagnitude = 90f;
-    int rotateDirection = 1; // -1 for Right, 1 for Left, 0 for Not Rotating
-
-    public GameObject player;
-
-    float timeElapsed;
-    readonly float startValue = 0;
-    float valueToLerp;
-
-    bool rotating = false;
-    float prerotationZ;
+    int direction = 0; //-1 Left, 1 Right
+    [SerializeField] float rotationSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.RightArrow) ^ Input.GetKeyDown(KeyCode.LeftArrow)) && !rotating)
+        if(0 > Input.GetAxisRaw("Horizontal"))
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                rotateDirection = -1;
-            }
-            else
-            {
-                rotateDirection = 1;
-            }
-
-            // Shut of Gravity & Unfreeze Rotation
-            Rigidbody2D[] bodies = GetComponentsInChildren<Rigidbody2D>();
-            foreach(Rigidbody2D body in bodies)
-            {
-                body.simulated = false;         
-            }
-
-            timeElapsed = 0f;
-            rotating = true;
-            prerotationZ = transform.eulerAngles.z;
+            direction = 1;
+        } else if(0 < Input.GetAxisRaw("Horizontal"))
+        {
+            direction = -1;
+        } else
+        {
+            direction = 0;
         }
 
-        if (rotating)
-        {
-            rotationLerp();
-        }
-    }
+        float zRotation = rotationSpeed * direction * Time.deltaTime;
+        
+        this.transform.Rotate(new Vector3(0, 0, zRotation));
 
-    // Method for Lerp Rotation
-    void rotationLerp()
-    {
-        if (timeElapsed < lerpDuration)
-        {
-            valueToLerp = Mathf.Lerp(startValue, endValueMagnitude, timeElapsed / lerpDuration);
-            timeElapsed += Time.deltaTime;
-        }
-        else // Runs When Lerp is About to End
-        {
-            valueToLerp = endValueMagnitude;
-            rotating = false;
-            
-            //Reenable Gravity
-            Rigidbody2D[] bodies = GetComponentsInChildren<Rigidbody2D>();
-            foreach (Rigidbody2D body in bodies)
-            {
-                body.velocity = Vector2.zero;
-                body.simulated = true;
-            }
 
-        }
-
-        transform.rotation = Quaternion.AngleAxis(prerotationZ + (rotateDirection * valueToLerp), Vector3.forward);
     }
 }
