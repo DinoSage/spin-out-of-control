9d402c505f627f20b6d4ff25171f6bd4e86488c3
DinoSage <dinosage5632@gmail.com>
2023-02-14 00:40:59 -0500

    Fixed Bad Resolution / Gameplay

--- Assets/Scripts/LevelMenuManager.cs
+++ Assets/Scripts/LevelMenuManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public class LevelMenuManager : MonoBehaviour
 {
@@ -62,6 +64,16 @@ public class LevelMenuManager : MonoBehaviour
 
         // Load levels
         spritearray = Resources.LoadAll<Sprite>(lvlLocation);
+        //List<Sprite> array = spritearray.OrderBy(sprite => sprite.name).ToList();
+        spritearray = Sort(spritearray);
+        //spritearray.Sort(spritearray, function(g1, g2) String.Compare(g1.name, g2.name));
+
+        foreach (Sprite s in spritearray)
+        {
+            Debug.Log(s.name);
+        }
+
+        //spritearray = Sort(spritearray);
         int levelCount = spritearray.Length + DEBUG_EXTRA_LEVELS;
 
         // Create Level Selector Buttons
@@ -151,5 +163,62 @@ public class LevelMenuManager : MonoBehaviour
         }
 
         yield return null;
+    }
+
+    private Sprite[] Sort(Sprite[] array)
+    {
+        // Create empty array
+        Sprite[] arr = new Sprite[array.Length];
+
+        Debug.Log(array.Length);
+        for (int i = 0; i < array.Length; i++)
+        {
+            // Current & Next Sprite
+            Sprite curr = array[i];
+            //int currNum = int.Parse(curr.name.Substring(curr.name.IndexOf("#") + 1));
+            //int nextNum = int.Parse(next.name.Substring(next.name.IndexOf("#") + 1));
+            //Debug.Log("Before: " + currNum + "\t After: " + nextNum);
+
+            int currNum = int.Parse(Regex.Match(array[i].name, @"\d+").Value);
+            //int nextNum = int.Parse(Regex.Match(array[i + 1].name, @"\d+").Value);
+
+            arr[currNum - 1] = array[i];
+
+            //Debug.Log("Before: " + currNum + "\t After: " + nextNum);
+
+            //Debug.Log("i: " + i);
+
+            /*int j = i;
+            bool notdone = true;
+            while  (j >= 0 && notdone)
+            {
+                Debug.Log("J: " + j);
+                //Debug.Log("Before: " + array[j].name + "\t After: " + array[j+1].name + "\t Compare: ");
+                //int currNum = int.Parse(array[j].name.Substring(array[j].name.IndexOf("#") + 1));
+                //int nextNum = int.Parse(array[j + 1].name.Substring(array[j + 1].name.IndexOf("#") + 1));
+
+                int currNum = int.Parse(Regex.Match(array[j].name, @"\d+").Value);
+                int nextNum = int.Parse(Regex.Match(array[j + 1].name, @"\d+").Value);
+
+
+                if (currNum < nextNum)
+                {
+                    notdone = false;
+                } else
+                {
+                    Sprite temp = array[i];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                }
+
+                j--;
+            }
+            */
+        }
+
+        Debug.Log(arr.Length);
+
+        return arr;
+
     }
 }
